19f6c5c0d3ddafed536ff7312f1b1cd9fde44851
Sahil Verma <domainsahil@gmail.com>
2022-02-12 10:29:49 +0530

    minor fix

--- client/components/OpenGraph.jsx
+++ client/components/OpenGraph.jsx
@@ -8,13 +8,13 @@ const OpenGraphs = ({ title, description, image, url }) => {
       <meta name="description" content={description} />
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={`${title} - Naked Angels`} />
+      <meta property="og:title" content={`${title} - Wallpaper Dowloader`} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta name="twitter:card" content={image} />
       <meta property="twitter:domain" content={url} />
       <meta property="twitter:url" content={url} />
-      <meta name="twitter:title" content={`${title} - Naked Angels`} />
+      <meta name="twitter:title" content={`${title} - Wallpaper Dowloader`} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
     </Head>
